144bd920c13b717d28846402ba4033a7cc9218b2
Aiza <sar1ykova00@gmail.com>
2023-11-26 00:20:01 +0600

    CODE

--- list.py
+++ list.py
@@ -1,1 +1,24 @@
-print('Hello')
+def my_project():
+    locations = [0, 0, 0]  
+    weights = [0, 0, 0]    
+    total_weight = 0          
+
+    while total_weight != 713:
+        print("\n Where the boxes could be buried?")
+        
+        for i in range(3):
+            kilometer_mark = int(input(f"Box {i + 1}: "))
+            
+            locations[i] = (locations[i] + kilometer_mark) % 10
+            weights[i] = int(input(f"Weight at {locations[i]} km: "))
+        
+        total_weight = sum(weights)
+
+        if total_weight == 713:
+            print("\n You found all the boxes.")
+            print("Cargo locations:", locations)
+            print("Cargo weights:", weights)
+        else:
+            print("\n The total weight is incorrect. TRY AGAIN!.")
+
+my_project()
